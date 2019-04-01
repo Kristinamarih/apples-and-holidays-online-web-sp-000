1c51dbe96786d7426d413d4fcbf84f6b7d3e1b66
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-04-01 01:58:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday.collect do |holiday, item|
      holiday if item.include?("BBQ")
    end
- end
+ end.flatten.compact
 end
 all_holidays_with_bbq(holiday_hash)
   
