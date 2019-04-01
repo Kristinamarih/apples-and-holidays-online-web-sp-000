65710ce3a62693545ab38d8e5ef3673185d7e883
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-04-01 01:44:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,9 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, item|
-      if item.include "BBQ"
+      if item.include?("BBQ")
+    end
+    return 
         
 end
 
