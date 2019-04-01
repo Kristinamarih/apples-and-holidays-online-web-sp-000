7f12bb81700ae9f2554dcb8fea22e985dbb91f4b
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-04-01 01:30:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,6 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
     
     holiday_hash.each do |holiday, item|
       puts "#{holiday.split(" ").capitalize.collect}: #{item.capitalize}"
+    end
   end
 end
 
